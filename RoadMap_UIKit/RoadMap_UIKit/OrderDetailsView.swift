f46ed606ff0a3810ac509279dfd909e024faa29c
Renbern <ArthurRenbern@icloud.com>
2022-09-23 01:38:16 +0300

    Сделал 4 домашку
    В качестве новой фичи сделал экран заказа доставки

---
+++ RoadMap_UIKit/RoadMap_UIKit/OrderDetailsView.swift
@@ -0,0 +1,41 @@
+//
+//  OrderDetailsView.swift
+//  RoadMap_UIKit
+//
+//  Created by Артур Щукин on 23.09.2022.
+//
+
+import UIKit
+
+/// Второй экран приложения
+class OrderDetailsView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Cafe Bistro"
+    }
+    @IBAction func getCheckButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Выставить чек?", message: "", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            let getCheck = UIStoryboard(name: "Main", bundle: nil)
+            guard let vc = getCheck.instantiateViewController(
+                withIdentifier: "CheckViewController"
+            ) as? CheckViewController else {
+                      return
+                  }
+            self.show(vc, sender: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func deliveryButton(_ sender: Any) {
+        let delivery = UIStoryboard(name: "Main", bundle: nil)
+          guard let vc = delivery.instantiateViewController(withIdentifier: "OrderDelivery") as? OrderDelivery
+        else {
+              return
+          }
+        self.show(vc, sender: nil)
+    }
+}
--- RoadMap_UIKit/RoadMap_UIKit/ViewController.swift
+++ RoadMap_UIKit/RoadMap_UIKit/ViewController.swift
@@ -12,4 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func signIn(_ sender: Any) {
+        let buttonSignIn = UIStoryboard(name: "Main", bundle: nil)
+          guard let vc = buttonSignIn.instantiateViewController(withIdentifier: "OrderDetailsView") as? OrderDetailsView
+        else {
+              return
+          }
+        self.show(vc, sender: nil)
+    }
 }
